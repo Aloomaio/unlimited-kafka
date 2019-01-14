56e3b335f96522df1cb9c2c45ca59313e9a5dfa3
Noa k <noak@alooma.io>
2019-01-14 11:12:31 +0200

    Update S3KeyGeneratorTest.java

--- src/test/java/com/alooma/unlimited_kafka/packer/s3/S3KeyGeneratorTest.java
+++ src/test/java/com/alooma/unlimited_kafka/packer/s3/S3KeyGeneratorTest.java
@@ -2,15 +2,63 @@ package com.alooma.unlimited_kafka.packer.s3;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class S3KeyGeneratorTest {
 
 
+    private final Pattern defaultPattern = Pattern.compile(".*\\d{4}/\\d{2}/\\d{2}/\\d{2}/.*/[a-z0-9\\-]{36}");
+    private final Pattern pattern = Pattern.compile(".*\\d{2}/\\w{3}/\\d{2}/\\d{2}/.*/[a-z0-9\\-]{36}");
+
     @Test
     void testKeyGenerator() {
 
-        
+        String topic = "test-topic";
+        S3ManagerParams s3ManagerParams = new S3ManagerParamsBuilder()
+                .withDirectoryNamePrefix("noa")
+                .withDirectoryNameDateTimeFormatter(DateTimeFormatter.ofPattern("yy'/'MMM'/'D'/'HH"))
+                .build();
+        S3KeyGenerator keyGenerator = new S3KeyGenerator(s3ManagerParams);
+
+        String generatedKey = keyGenerator.genrate(topic);
+
+        assertTrue(pattern.matcher(generatedKey).matches());
 
     }
+
+    @Test
+    void testKeyGenerator_defaultDateTimeFormat() {
+
+        String topic = "test-topic";
+        S3ManagerParams s3ManagerParams = new S3ManagerParamsBuilder()
+                .withDirectoryNamePrefix("noa")
+                .build();
+        S3KeyGenerator keyGenerator = new S3KeyGenerator(s3ManagerParams);
+
+        String generatedKey = keyGenerator.genrate(topic);
+
+        assertTrue(defaultPattern.matcher(generatedKey).matches());
+
+    }
+
+    @Test
+    void testKeyGenerator_withoutPrefix() {
+
+        String topic = "test-topic";
+        S3ManagerParams s3ManagerParams = new S3ManagerParamsBuilder()
+                .build();
+        S3KeyGenerator keyGenerator = new S3KeyGenerator(s3ManagerParams);
+
+        String generatedKey = keyGenerator.genrate(topic);
+
+        assertTrue(defaultPattern.matcher(generatedKey).matches());
+
+    }
+
+    //todo: test gz
+
+
 }
